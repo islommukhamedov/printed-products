e7c578e813e943597b087d5da2b1d8b55247ec40
islommukhamedov <115744626+islommukhamedov@users.noreply.github.com>
2024-01-08 22:03:54 +0500

    Update BaseController.java

--- src/main/java/com/company/controller/BaseController.java
+++ src/main/java/com/company/controller/BaseController.java
@@ -9,3 +9,15 @@ public interface BaseController {
 
     void filterByPrice();
 }
+
+public void addNewBase() {
+    BaseUtils.println("Enter base details:");
+}
+
+public void deleteBase() {
+    BaseUtils.println("Enter base ID to delete:");
+}
+
+public void addNewCategory() {
+    BaseUtils.println("Enter new category name:");
+}
